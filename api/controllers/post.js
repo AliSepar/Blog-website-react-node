d358ad8c2be75f0928489097a244cb45f79177f1
AliSepar <alisabah.separ1@gmail.com>
2024-11-13 14:17:04 +0100

    display posts by catagory & single post

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -1,3 +1,37 @@
+import { db } from "../db.js";
+
+export const getPosts = (req, res) => {
+  const q = req.query.cat
+    ? "SELECT * FROM posts where catagory = ?"
+    : "SELECT * FROM posts";
+
+  db.query(q, [req.query.cat], (err, data) => {
+    if (err) return res.send(err);
+
+    //if no error
+    return res.status(200).json(data);
+  });
+};
+export const getPost = (req, res) => {
+  const q =
+    "SELECT `username`, `title`, `descrption`, `catagory` ,p.img, u.img as userImg ,`date` from user u join posts p ON u.id = p.uid WHERE p.id =?";
+
+  db.query(q, [req.params.id], (err, data) => {
+    if (err) return res.status(500).json(err);
+
+    return res.status(200).json(data[0]);
+    // becouse we fetch only one item so we send one item in array res
+  });
+};
+
 export const addPost = (req, res) => {
+  res.json("from post controller");
+};
+
+export const deletePost = (req, res) => {
+  res.json("from post controller");
+};
+
+export const updatePost = (req, res) => {
   res.json("from post controller");
 };
--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,77 +1,75 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import Edit from "../assets/img/edit.png";
 import Delete from "../assets/img/delete.png";
-import { Link } from "react-router-dom";
 import Menu from "../components/Menu";
+import axios from "axios";
+import moment from "moment";
+import { AuthContext } from "../context/authContext";
 
 function Single() {
+  const [post, setPost] = useState({});
+
+  const { currentUser } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  // getting url there are other data in the useLocation
+  const location = useLocation();
+  // getting post id from the url
+  const postId = location.pathname.split("/")[2];
+
+  const handleDelete = async () => {
+    try {
+      await axios.delete(`/api/posts/${postId}`);
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(`/api/posts/${postId}`);
+        setPost(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, [postId]);
+
+  console.log(post);
   return (
     <div className="single">
       <div className="content">
-        <img
-          src="https://images.pexels.com/photos/7008010/pexels-photo-7008010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-          alt=""
-        />
+        <img src={post?.img} alt="post Image" />
         <div className="user">
           <img
-            src="https://images.pexels.com/photos/7008010/pexels-photo-7008010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            src={
+              post.userImg ||
+              "https://images.pexels.com/photos/7008010/pexels-photo-7008010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            }
             alt=""
           />
           <div className="info">
             <span>John</span>
-            <p>Posted 2 days ago</p>
+            <p>Posted {moment(post.date).fromNow}</p>
+            {/* moment will show the deffrence bettween the data from now */}
           </div>
-          <div className="edit">
-            <Link to={`/write?edit=2`}>
-              <img src={Edit} alt="" />
-            </Link>
-            <img src={Delete} alt="" />
-          </div>
+          {currentUser.username === post.username && (
+            <div className="edit">
+              <Link to={`/write?edit=2`}>
+                <img src={Edit} alt="" />
+              </Link>
+              <img onClick={handleDelete} src={Delete} alt="" />
+            </div>
+          )}
         </div>
         {/* content */}
-        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis
-          ad voluptatibus sit placeat ipsum necessitatibus, sequi molestiae eius
-          ab ipsam voluptates. Officia perferendis sint, possimus eaque odit
-          aspernatur accusantium blanditiis. Lorem ipsum dolor sit amet.
-          <br />
-          <br />
-          consectetur adipisicing elit. Minus sint modi, illum numquam repellat
-          placeat voluptate quam ducimus, nisi distinctio, nemo eaque amet
-          soluta deleniti? Corrupti consequuntur ab iste obcaecati! consectetur
-          adipisicing elit. Minus sint modi, illum numquam repellat placeat
-          voluptate quam ducimus, nisi distinctio, nemo eaque amet soluta
-          deleniti? Corrupti consequuntur ab iste obcaecati! consectetur
-          adipisicing elit. Minus sint modi, illum numquam repellat placeat
-          voluptate quam ducimus, nisi distinctio, nemo eaque amet soluta
-          deleniti? Corrupti consequuntur ab iste obcaecati! consectetur
-          adipisicing elit. Minus sint modi, illum numquam repellat placeat
-          voluptate quam ducimus, nisi distinctio, nemo eaque amet soluta
-          deleniti? Corrupti consequuntur ab iste obcaecati!
-          <br />
-          <br />
-          consectetur adipisicing elit. Minus sint modi, illum numquam repellat
-          placeat voluptate quam ducimus, nisi distinctio, nemo eaque amet
-          soluta deleniti? Corrupti consequuntur ab iste obcaecati! consectetur
-          adipisicing elit. Minus sint modi, illum numquam repellat placeat
-          voluptate quam ducimus, nisi distinctio, nemo eaque amet soluta
-          deleniti? Corrupti consequuntur ab iste obcaecati! consectetur
-          adipisicing elit. Minus sint modi, illum numquam repellat placeat
-          voluptate quam ducimus, nisi distinctio, nemo eaque amet soluta
-          deleniti? Corrupti consequuntur ab iste obcaecati! consectetur
-          adipisicing elit. Minus sint modi, illum numquam repellat placeat
-          voluptate quam ducimus, nisi distinctio, nemo eaque amet soluta
-          deleniti? Corrupti consequuntur ab iste obcaecati!
-          <br />
-          <br />
-          consectetur adipisicing elit. Minus sint modi, illum numquam repellat
-          placeat voluptate quam ducimus, nisi distinctio, nemo eaque amet
-          soluta deleniti? Corrupti consequuntur ab iste obcaecati! consectetur
-          adipisicing elit. Minus sint modi, illum numquam repellat placeat
-          voluptate quam ducimus, nisi distinctio, nemo eaque amet soluta
-          deleniti? Corrupti consequuntur ab iste obcaecati!
-        </p>
+        <h1>{post?.title}</h1>
+        {post?.descrption}
       </div>
       <Menu />
     </div>
